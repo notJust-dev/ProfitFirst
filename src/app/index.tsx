ef2db381072cd48c32ec5a4431a40b8976d87b3a
Vadim Savin <savinvadim1312@gmail.com>
2024-04-19 15:26:20 +0100

    setup expo router

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Text>Hello world</Text>
+      <Text>Home screen</Text>
       <StatusBar style="auto" />
     </View>
   );
