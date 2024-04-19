7e1f2e7e35f548258754c0ac954afd398bfbc02e
Vadim Savin <savinvadim1312@gmail.com>
2024-04-19 18:01:42 +0100

    delete account

--- src/components/AccountListItem.tsx
+++ src/components/AccountListItem.tsx
@@ -1,17 +1,26 @@
 import { View, Text, StyleSheet } from 'react-native';
 import Account from '../model/Account';
 import { withObservables } from '@nozbe/watermelondb/react';
+import { AntDesign } from '@expo/vector-icons';
+import database from '../db';
 
 type AccountListItem = {
   account: Account;
 };
 
 function AccountListItem({ account }: AccountListItem) {
+  const onDelete = async () => {
+    await database.write(async () => {
+      await account.markAsDeleted();
+    });
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.name}>{account.name}</Text>
       <Text style={styles.percentage}>{account.cap}%</Text>
       <Text style={styles.percentage}>{account.tap}%</Text>
+      <AntDesign name="delete" size={18} color="gray" onPress={onDelete} />
     </View>
   );
 }
@@ -36,6 +45,9 @@ const styles = StyleSheet.create({
   name: {
     fontWeight: 'bold',
     fontSize: 16,
+    flex: 1,
   },
-  percentage: {},
+  percentage: {
+    flex: 1,
+  },
 });
